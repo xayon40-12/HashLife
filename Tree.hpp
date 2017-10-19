cdca212175e5f02696be6b2481e5d2205dc310b9
Touroux Nathan <touroux.nathan@gmail.com>
2017-10-19 20:45:47 +0200

    move definitions from Tree<A> in Tree.hpp to Tree.inl (more readable)

--- Tree.hpp
+++ Tree.hpp
@@ -17,280 +17,73 @@ template<class A>
 class Tree{
 private:
     static std::unordered_set<Tree<A>> trees;
-    static std::unordered_map<unsigned int, Tree> memoizedEmpty;
-    static std::unordered_map<Tree, Tree> momoizedNext;
+    static std::unordered_map<unsigned int, Tree<A>> memoizedEmpty;
+    static std::unordered_map<Tree<A>, Tree<A>> momoizedNext;
 
-    const Tree *nw, *ne, *sw, *se;
+    const Tree<A> *nw, *ne, *sw, *se;
     A value;
     unsigned int level;
     long pow;//2^(level-1)
 
-    Tree center() const {
-        return {*nw->se, *ne->sw, *sw->ne, *se->nw};
-    }
-    Tree Hcenter(Tree const *w, const Tree *e) const {
-        return {*w->ne, *e->nw, *w->se, *e->sw};
-    }
-    Tree Vcenter(Tree const *n, Tree const *s) const {
-        return {*n->sw, *n->se, *s->nw, *s->ne};
-    }
+    Tree<A> center() const;
+    Tree<A> Hcenter(Tree<A> const *w, const Tree<A> *e) const;
+    Tree<A> Vcenter(Tree<A> const *n, Tree<A> const *s) const;
 
-    Tree set(long tabX, long tabY, Tree t) {
-        if(tabY == 0){// N
-            if(tabX == 0)// W
-                return Tree(t, *ne, *sw, *se);
-            else// E
-                return Tree(*nw, t, *sw, *se);
-        }else{// S
-            if(tabX == 0)// W
-                return Tree(*nw, *ne, t, *se);
-            else// E
-                return Tree(*nw, *ne, *sw, t);
-        }
-    }
+    Tree<A> set(long tabX, long tabY, Tree<A> t);
 
 public:
-    Tree(A value): nw(nullptr), ne(nullptr), sw(nullptr), se(nullptr), value(value), level(0), pow(0) {
+    Tree(A value);
+    Tree(Tree<A> nw, Tree<A> ne, Tree<A> sw, Tree<A> se);
+    Tree(Tree<A> const &t);
+    Tree<A>& operator=(Tree<A> const &t);
+    ~Tree<A>();
 
-    }
-    Tree(Tree nw, Tree ne, Tree sw, Tree se): nw(nullptr), ne(nullptr), sw(nullptr), se(nullptr), value(), level(nw.level+1), pow(nw.pow==0?1:2*nw.pow)  {
-        this->nw = &(*trees.insert(nw).first);
-        this->ne = &(*trees.insert(ne).first);
-        this->sw = &(*trees.insert(sw).first);
-        this->se = &(*trees.insert(se).first);
-    }
-    Tree(Tree const &t): nw(t.nw), ne(t.ne), sw(t.sw), se(t.se), value(t.value), level(t.level), pow(t.pow) {
+    bool operator==(Tree<A> const &t) const;
 
-    }
-    Tree& operator=(Tree const &t) {
-        nw = t.nw;
-        ne = t.ne;
-        sw = t.sw;
-        se = t.se;
-        value = t.value;
-        level = t.level;
-        pow = t.pow;
-        return *this;
-    }
-    ~Tree() = default;
+    A getValue() const;
+    int getLevel() const;
+    bool isLeaf() const;
 
-    bool operator==(Tree const &t) const {
-        return (nw == t.nw &&
-                ne == t.ne &&
-                sw == t.sw &&
-                se == t.se &&
-                value == t.value &&
-                level == t.level);
-    }
+    const Tree<A> *getNW() const;
+    const Tree<A> *getNE() const;
+    const Tree<A> *getSW() const;
+    const Tree<A> *getSE() const;
 
-    A getValue() const {
-        return value;
-    }
-    int getLevel() const {
-        return level;
-    }
-    bool isLeaf() const {
-        return (nw == nullptr &&
-                ne == nullptr &&
-                sw == nullptr &&
-                se == nullptr);
-    }
+    static Tree<A> generate(unsigned int level, const A &a = A());
 
-    const Tree *getNW() const {
-        return nw;
-    }
-    const Tree *getNE() const {
-        return ne;
-    }
-    const Tree *getSW() const {
-        return sw;
-    }
-    const Tree *getSE() const {
-        return se;
-    }
+    Tree<A> expend(const A &a = A());
 
-    static Tree generate(unsigned int level, const A &a = A()) {
-        auto memo = memoizedEmpty.find(level);
-        if(memo != memoizedEmpty.end())
-            return memo->second;
+    Tree<A> expend(unsigned long size, const A &a = A());
 
-        if (level == 0) {
-            return memoizedEmpty.insert({level, Tree(a)}).first->second;
-        } else {
-            return memoizedEmpty.insert({level, {generate(level - 1, a), generate(level - 1, a), generate(level - 1, a),
-                                                 generate(level - 1, a)}}).first->second;
-        }
-    }
+    std::vector<std::vector<const Tree<A>*>> getSubTrees();
 
-    Tree expend(const A &a = A()) {
-        if(isLeaf()){
-            Tree e = generate(level, a);
-            return Tree(*this, e, e, e);
-        }else{
-            Tree e = generate(level - 1, a);
-            return Tree(Tree(e, e, e, *nw),
-                        Tree(e, e, *ne, e),
-                        Tree(e, *sw, e, e),
-                        Tree(*se, e, e, e));
-        }
-    }
+    A get(long x, long y);
 
-    Tree expend(unsigned long size, const A &a = A()){
-        if(size == 1)
-            return expend(a);
-        else
-            return expend(size-1, a).expend(a);
-    }
+    std::vector<std::vector<A>> getRect(long x1, long y1, long x2, long y2);
 
-    std::vector<std::vector<const Tree*>> getSubTrees() {
-        return {{nw, ne},{sw, se}};
-    }
+    std::vector<std::vector<A>> get();
 
-    A get(long x, long y) {
-        //   y/\
-        //    |
-        //–––––––––>x
-        //    |
-        //    |
-        Tree const *t(this);
-        long px = 0, py = 0;
-        long pow = getPow();
-        while(t->getLevel() != 0){
-            pow /= 2;
-            if(x >= px && y >= py){
-                t = t->getNE();
-                px += pow;
-                py += pow;
-            } else if(x >= px && y < py){
-                t = t->getSE();
-                px += pow;
-                py -= pow;
-            } else if(x < px && y >= py){
-                t = t->getNW();
-                px -= pow;
-                py += pow;
-            } else if(x < px && y < py){
-                t = t->getSW();
-                px -= pow;
-                py -= pow;
-            }
-        }
-        return t->getValue();
-    }
+    Tree<A> nextGeneration();
 
-    std::vector<std::vector<A>> getRect(long x1, long y1, long x2, long y2) {
-        std::vector<std::vector<A>> values;
-        if(x1>x2) std::swap(x1, x2);
-        if(y1<y2) std::swap(y1, y2);
-        long dx = x2 - x1, dy = y1 - y2;
-        for(long y = 0;y<=dy;y++){
-            values.push_back(std::vector<A>());
-            for(long x = 0;x<=dx;x++){
-                values[y].push_back(get(x1+x, y1-y));
-            }
-        }
-        return values;
-    }
+    long getPow() const;
 
-    std::vector<std::vector<A>> get() {
-        return getRect(-pow, pow-1, pow-1, -pow);
-    }
+    void show(long x1, long y1, long x2, long y2);
 
-    Tree nextGeneration() {
-        if(level < 2)
-            return *this;//can't update
+    void show();
+    void show(long l);
 
-        auto memo = momoizedNext.find(*this);
-        if(memo != momoizedNext.end())
-            return memo->second;
+    Tree<A> set(long x, long y, A const &a, long px = 0, long py = 0);
 
-        if(level-1 == A::detectionLength()){
+    Tree<A> set(long x1, long y1, std::vector<std::vector<A>> tab);
 
-            auto tab = get(), tab2 = tab;
-            for(long y = 1;y<=2;y++){
-                for(long x = 1;x<=2;x++){
-                    tab[y][x].update(tab2, x, y);
-                }
-            }
-            return Tree(tab[1][1],tab[1][2],tab[2][1],tab[2][2]);
-        }else{
-            Tree t00 = nw->center();
-            Tree t01 = Hcenter(nw, ne).center();
-            Tree t02 = ne->center();
-            Tree t10 = Vcenter(nw, sw).center();
-            Tree t11 = center().center();
-            Tree t12 = Vcenter(ne, se).center();
-            Tree t20 = sw->center();
-            Tree t21 = Hcenter(sw, se).center();
-            Tree t22 = se->center();
+    Tree<A> set(std::vector<std::vector<A>> tab);
 
-            return momoizedNext.insert({*this, Tree(Tree(t00, t01, t10, t11).nextGeneration(),
-                                                    Tree(t01, t02, t11, t12).nextGeneration(),
-                                                    Tree(t10, t11, t20, t21).nextGeneration(),
-                                                    Tree(t11, t12, t21, t22).nextGeneration())}).first->second;
-        }
-    }
-
-    long getPow() const {
-        return pow;
-    }
-
-    void show(long x1, long y1, long x2, long y2) {
-        if(x1>x2) std::swap(x1, x2);
-        if(y1<y2) std::swap(y1, y2);
-        auto values = getRect(x1, y1, x2, y2);
-        long dx = x2 - x1, dy = y1 - y2;
-        std::ostringstream os;
-        for(long y = 0;y<=dy;y++){
-            for(long x = 0;x<=dx;x++){
-                values[y][x].show(x, y);
-            }
-        }
-    }
-
-    void show() {
-        show(-pow, pow-1, pow-1, -pow);
-    }
-    void show(long l) {
-        show(-l, l-1, l-1, -l);
-    }
-
-    Tree set(long x, long y, A const &a, long px = 0, long py = 0) {
-        if(level == 0)
-            return (getValue() == a)?*this:Tree(a);
-        else{
-            long dx = (x >= px), dy = (y >= py);
-            Tree t = *getSubTrees()[!dy][dx];//!dy because y axis is upside down in the array
-            return set(dx, !dy, t.set(x, y, a, px+(dx?1:-1)*t.getPow(), py+(dy?1:-1)*t.getPow()));
-        }
-    }
-
-    Tree set(long x1, long y1, std::vector<std::vector<A>> tab) {
-        long dx = tab[0].size()-1, dy = tab.size()-1;
-        Tree t = *this;
-        for(long y = 0;y<=dy;y++){
-            for(long x = 0;x<=dx;x++){
-                t = t.set(x1+x, y1-y, tab[y][x]);
-            }
-        }
-        return t;
-    }
-
-    Tree set(std::vector<std::vector<A>> tab){
-        return set(-(tab[0].size()/2), tab.size()/2, tab);
-    }
-
-    static void reset(){
-        trees.clear();
-        memoizedEmpty.clear();
-        momoizedNext.clear();
-    }
+    static void reset();
 };
 
-template<class A> std::unordered_set<Tree<A>> Tree<A>::trees;
-template<class A> std::unordered_map<unsigned int, Tree<A>> Tree<A>::memoizedEmpty;
-template<class A> std::unordered_map<Tree<A>, Tree<A>> Tree<A>::momoizedNext;
+#include "Tree.inl"
 
+//******************************************** MAKE HASHABLE ********************************************
 MAKE_HASHABLE_T(Tree, t.getSE(), t.getSW(), t.getNE(), t.getNW(), t.getLevel(), t.getValue())
 
 #endif //HASHLIFE_TREE_HPP
---
+++ Tree.inl
@@ -0,0 +1,269 @@
+//template defenition of class Tree<A> from Tree.hpp
+
+//******************************************** PRIVATE ********************************************
+template<class A> std::unordered_set<Tree<A>> Tree<A>::trees;
+template<class A> std::unordered_map<unsigned int, Tree<A>> Tree<A>::memoizedEmpty;
+template<class A> std::unordered_map<Tree<A>, Tree<A>> Tree<A>::momoizedNext;
+
+template<class A> Tree<A> Tree<A>::center() const {
+    return {*nw->se, *ne->sw, *sw->ne, *se->nw};
+}
+template<class A> Tree<A> Tree<A>::Hcenter(Tree<A> const *w, const Tree<A> *e) const {
+    return {*w->ne, *e->nw, *w->se, *e->sw};
+}
+template<class A> Tree<A> Tree<A>::Vcenter(Tree<A> const *n, Tree<A> const *s) const {
+    return {*n->sw, *n->se, *s->nw, *s->ne};
+}
+
+template<class A> Tree<A> Tree<A>::set(long tabX, long tabY, Tree<A> t) {
+    if(tabY == 0){// N
+        if(tabX == 0)// W
+            return Tree<A>(t, *ne, *sw, *se);
+        else// E
+            return Tree<A>(*nw, t, *sw, *se);
+    }else{// S
+        if(tabX == 0)// W
+            return Tree<A>(*nw, *ne, t, *se);
+        else// E
+            return Tree<A>(*nw, *ne, *sw, t);
+    }
+}
+
+//******************************************** PUBLIC ********************************************
+
+template<class A> Tree<A>::Tree(A value): nw(nullptr), ne(nullptr), sw(nullptr), se(nullptr), value(value), level(0), pow(0) {
+
+}
+template<class A> Tree<A>::Tree(Tree<A> nw, Tree<A> ne, Tree<A> sw, Tree<A> se): nw(nullptr), ne(nullptr), sw(nullptr), se(nullptr), value(), level(nw.level+1), pow(nw.pow==0?1:2*nw.pow)  {
+    this->nw = &(*trees.insert(nw).first);
+    this->ne = &(*trees.insert(ne).first);
+    this->sw = &(*trees.insert(sw).first);
+    this->se = &(*trees.insert(se).first);
+}
+template<class A> Tree<A>::Tree(Tree<A> const &t): nw(t.nw), ne(t.ne), sw(t.sw), se(t.se), value(t.value), level(t.level), pow(t.pow) {
+
+}
+template<class A> Tree<A>& Tree<A>::operator=(Tree<A> const &t) {
+    nw = t.nw;
+    ne = t.ne;
+    sw = t.sw;
+    se = t.se;
+    value = t.value;
+    level = t.level;
+    pow = t.pow;
+    return *this;
+}
+
+template<class A> Tree<A>::~Tree<A>() = default;
+
+template<class A> bool Tree<A>::operator==(Tree<A> const &t) const {
+    return (nw == t.nw &&
+            ne == t.ne &&
+            sw == t.sw &&
+            se == t.se &&
+            value == t.value &&
+            level == t.level);
+}
+
+template<class A> A Tree<A>::getValue() const {
+    return value;
+}
+template<class A> int Tree<A>::getLevel() const {
+    return level;
+}
+template<class A> bool Tree<A>::isLeaf() const {
+    return (nw == nullptr &&
+            ne == nullptr &&
+            sw == nullptr &&
+            se == nullptr);
+}
+
+template<class A> const Tree<A> *Tree<A>::getNW() const {
+    return nw;
+}
+template<class A> const Tree<A> *Tree<A>::getNE() const {
+    return ne;
+}
+template<class A> const Tree<A> *Tree<A>::getSW() const {
+    return sw;
+}
+template<class A> const Tree<A> *Tree<A>::getSE() const {
+    return se;
+}
+
+template<class A> Tree<A> Tree<A>::generate(unsigned int level, const A &a) {
+    auto memo = memoizedEmpty.find(level);
+    if(memo != memoizedEmpty.end())
+        return memo->second;
+
+    if (level == 0) {
+        return memoizedEmpty.insert({level, Tree<A>(a)}).first->second;
+    } else {
+        return memoizedEmpty.insert({level, {generate(level - 1, a), generate(level - 1, a), generate(level - 1, a),
+                                             generate(level - 1, a)}}).first->second;
+    }
+}
+
+template<class A> Tree<A> Tree<A>::expend(const A &a) {
+    if(isLeaf()){
+        Tree<A> e = generate(level, a);
+        return Tree<A>(*this, e, e, e);
+    }else{
+        Tree<A> e = generate(level - 1, a);
+        return Tree<A>(Tree<A>(e, e, e, *nw),
+                       Tree<A>(e, e, *ne, e),
+                       Tree<A>(e, *sw, e, e),
+                       Tree<A>(*se, e, e, e));
+    }
+}
+
+template<class A> Tree<A> Tree<A>::expend(unsigned long size, const A &a){
+    if(size == 1)
+        return expend(a);
+    else
+        return expend(size-1, a).expend(a);
+}
+
+template<class A> std::vector<std::vector<const Tree<A>*>> Tree<A>::getSubTrees() {
+    return {{nw, ne},{sw, se}};
+}
+
+template<class A> A Tree<A>::get(long x, long y) {
+    //   y/\
+        //    |
+    //–––––––––>x
+    //    |
+    //    |
+    Tree<A> const *t(this);
+    long px = 0, py = 0;
+    long pow = getPow();
+    while(t->getLevel() != 0){
+        pow /= 2;
+        if(x >= px && y >= py){
+            t = t->getNE();
+            px += pow;
+            py += pow;
+        } else if(x >= px && y < py){
+            t = t->getSE();
+            px += pow;
+            py -= pow;
+        } else if(x < px && y >= py){
+            t = t->getNW();
+            px -= pow;
+            py += pow;
+        } else if(x < px && y < py){
+            t = t->getSW();
+            px -= pow;
+            py -= pow;
+        }
+    }
+    return t->getValue();
+}
+
+template<class A> std::vector<std::vector<A>> Tree<A>::getRect(long x1, long y1, long x2, long y2) {
+    std::vector<std::vector<A>> values;
+    if(x1>x2) std::swap(x1, x2);
+    if(y1<y2) std::swap(y1, y2);
+    long dx = x2 - x1, dy = y1 - y2;
+    for(long y = 0;y<=dy;y++){
+        values.push_back(std::vector<A>());
+        for(long x = 0;x<=dx;x++){
+            values[y].push_back(get(x1+x, y1-y));
+        }
+    }
+    return values;
+}
+
+template<class A> std::vector<std::vector<A>> Tree<A>::get() {
+    return getRect(-pow, pow-1, pow-1, -pow);
+}
+
+template<class A> Tree<A> Tree<A>::nextGeneration() {
+    if(level < 2)
+        return *this;//can't update
+
+    auto memo = momoizedNext.find(*this);
+    if(memo != momoizedNext.end())
+        return memo->second;
+
+    if(level-1 == A::detectionLength()){
+
+        auto tab = get(), tab2 = tab;
+        for(long y = 1;y<=2;y++){
+            for(long x = 1;x<=2;x++){
+                tab[y][x].update(tab2, x, y);
+            }
+        }
+        return Tree<A>(tab[1][1],tab[1][2],tab[2][1],tab[2][2]);
+    }else{
+        Tree<A> t00 = nw->center();
+        Tree<A> t01 = Hcenter(nw, ne).center();
+        Tree<A> t02 = ne->center();
+        Tree<A> t10 = Vcenter(nw, sw).center();
+        Tree<A> t11 = center().center();
+        Tree<A> t12 = Vcenter(ne, se).center();
+        Tree<A> t20 = sw->center();
+        Tree<A> t21 = Hcenter(sw, se).center();
+        Tree<A> t22 = se->center();
+
+        return momoizedNext.insert({*this, Tree<A>(Tree<A>(t00, t01, t10, t11).nextGeneration(),
+                                                   Tree<A>(t01, t02, t11, t12).nextGeneration(),
+                                                   Tree<A>(t10, t11, t20, t21).nextGeneration(),
+                                                   Tree<A>(t11, t12, t21, t22).nextGeneration())}).first->second;
+    }
+}
+
+template<class A> long Tree<A>::getPow() const {
+    return pow;
+}
+
+template<class A> void Tree<A>::show(long x1, long y1, long x2, long y2) {
+    if(x1>x2) std::swap(x1, x2);
+    if(y1<y2) std::swap(y1, y2);
+    auto values = getRect(x1, y1, x2, y2);
+    long dx = x2 - x1, dy = y1 - y2;
+    std::ostringstream os;
+    for(long y = 0;y<=dy;y++){
+        for(long x = 0;x<=dx;x++){
+            values[y][x].show(x, y);
+        }
+    }
+}
+
+template<class A> void Tree<A>::show() {
+    show(-pow, pow-1, pow-1, -pow);
+}
+template<class A> void Tree<A>::show(long l) {
+    show(-l, l-1, l-1, -l);
+}
+
+template<class A> Tree<A> Tree<A>::set(long x, long y, A const &a, long px, long py) {
+    if(level == 0)
+        return (getValue() == a)?*this:Tree<A>(a);
+    else{
+        long dx = (x >= px), dy = (y >= py);
+        Tree<A> t = *getSubTrees()[!dy][dx];//!dy because y axis is upside down in the array
+        return set(dx, !dy, t.set(x, y, a, px+(dx?1:-1)*t.getPow(), py+(dy?1:-1)*t.getPow()));
+    }
+}
+
+template<class A> Tree<A> Tree<A>::set(long x1, long y1, std::vector<std::vector<A>> tab) {
+    long dx = tab[0].size()-1, dy = tab.size()-1;
+    Tree<A> t = *this;
+    for(long y = 0;y<=dy;y++){
+        for(long x = 0;x<=dx;x++){
+            t = t.set(x1+x, y1-y, tab[y][x]);
+        }
+    }
+    return t;
+}
+
+template<class A> Tree<A> Tree<A>::set(std::vector<std::vector<A>> tab){
+    return set(-(tab[0].size()/2), tab.size()/2, tab);
+}
+
+template<class A> void Tree<A>::reset(){
+    trees.clear();
+    memoizedEmpty.clear();
+    momoizedNext.clear();
+}
